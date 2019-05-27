9b86cd20c80ab611c425bc43c062bf324721db69
unknown <ygl960630@163.com>
2019-05-27 10:16:17 +0800

    初始化项目

---
+++ scripts/test_001.py
@@ -0,0 +1,18 @@
+import allure, pytest
+
+
+class Test001:
+    @pytest.allure.severity(pytest.allure.severity_level.BLOCKER)
+    @allure.step(title="这是测试步骤01")
+    def test_001_1(self):
+        with open("./Snipaste.png","rb") as f:
+            allure.attach("截图",f.read(),allure.attach_type.PNG)
+        print("-->test_001_1")
+        assert True
+
+    @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
+    @allure.step(title="这是测试步骤02")
+    def test_001_2(self):
+        print("-->test_001_2")
+        allure.attach("标题", "具体内容")  # 添加描述信息
+        assert True
